cf4303c90e6f6ec5f42f9682cecba932b9276239
Alejandro Ortega <43015395+AlejoOrtega@users.noreply.github.com>
2022-06-12 18:44:45 -0400

    commit tree 31595fb57769692ec58a3b07de245fe052e9c019

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validate :check_category
+    validate :check_title
+
+    def check_category
+        if category!='Fiction' and category!='Non-Fiction'
+            errors.add(:category, 'is not included in the list')
+        end
+    end
+
+    def check_title
+        if title =='True Facts'
+            errors.add(:title, 'title is clickbait')
+        end
+    end
+
+
 end
